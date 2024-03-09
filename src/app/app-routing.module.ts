a4bc5b5526a8b8ecd0c278c8fb0f2d2485900b2d
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-03-09 11:23:20 +0100

    Configurazioni iniziali

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'campionati', loadChildren: () => import('./pages/campionati/campionati.module').then(m => m.CampionatiModule) }, { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
