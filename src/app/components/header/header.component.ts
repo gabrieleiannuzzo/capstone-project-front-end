3d7f45def6aab42555feb957249e9810a3f3b668
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-03-10 14:23:18 +0100

    Aggiunte funzionalità header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../pages/auth/auth.service';
+import { Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ILoginResponse } from '../../models/ilogin-response';
+import { environment } from '../../../environments/environment.development';
+import { IInvitoResponse } from '../../models/iinvito-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +13,49 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  isLogged:boolean = !false;
+  isLogged!:boolean;
+  user!:ILoginResponse|null;
+  inviti!:IInvitoResponse[];
   active:boolean = false;
-  notifications:boolean = false;
 
-  auth(){}
+  constructor(
+    private authService:AuthService,
+    private http:HttpClient,
+    private router:Router,
+  ){}
+
+  isLoggedSubscription!:Subscription;
+  userSubscription!:Subscription;
+
+  ngOnInit(){
+    this.isLoggedSubscription = this.authService.isLogged$.subscribe(data => this.isLogged = data);
+
+    this.userSubscription = this.authService.user$.subscribe(data => this.user = data);
+
+    this.getPendingNotifications();
+  }
+
+  auth(){
+    if (this.isLogged) {
+      this.authService.logout();
+    } else {
+      this.router.navigate(["/login"]);
+    }
+    this.toggleActive();
+  }
 
   toggleActive():void{
     this.active = !this.active;
+  }
+
+  getPendingNotifications():void{
+    if (!this.isLogged) return;
+    const username:string|undefined = this.user?.user.username;
+    const invitiUrl = environment.apiUrl + username + "/inviti";
+    this.http.get<IInvitoResponse[]>(invitiUrl).subscribe(data => this.inviti = data);
+  }
+
+  pendingNotifications():boolean{
+    return this.inviti.length > 0;
   }
 }
--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
